4ea6643556b6dc486144fa582d45195559b253d9
Gubin Zhao <gbzhao@outlook.com>
2024-03-13 21:06:08 +0000

    优化了文件结构 ```

---
+++ cipher/substitution_cipher.py
@@ -0,0 +1,103 @@
+from pycipher import SimpleSubstitution as SimpleSub
+import random
+import re
+from modules.ngram_score import ngram_score
+from modules.check_words import check_english
+fitness = ngram_score('data/quadgrams.txt') # load our quadgram statistics
+
+def encrypt(message, key, casesensitive=False):  # substitution cipher encrypt
+    # define the alphabets
+    alphabet_lower = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    dict_lower = {alphabet_lower[i]: key[i].lower() for i in range(26)}
+    dict_upper = {alphabet_upper[i]: key[i].upper() for i in range(26)}
+
+    encrypted_message = ""
+    for char in message:
+        if char.isalpha():
+            if casesensitive is False:
+                char = char.upper()
+            if char.isupper():
+                encrypted_message += dict_upper[char]
+            else:
+                encrypted_message += dict_lower[char]
+        else:
+            encrypted_message += char
+
+    return encrypted_message
+
+
+def decrypt(encrypted_message, key, casesensitive=False):  # substitution cipher decrypt with key
+    # define the alphabets
+    alphabet_lower = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    dict_lower = {key[i].lower(): alphabet_lower[i] for i in range(26)}
+    dict_upper = {key[i].upper(): alphabet_upper[i] for i in range(26)}
+
+    decrypted_message = ""
+    for char in encrypted_message:
+        if char.isalpha():
+            if casesensitive is False:
+                char = char.upper()
+            if char.isupper():
+                decrypted_message += dict_upper[char]
+            else:
+                decrypted_message += dict_lower[char]
+        else:
+            decrypted_message += char
+
+    return decrypted_message
+
+
+def decrypt_without_key(encrypted_message, casesensitive):  # substitution cipher decrypt without key
+    key = find_key(encrypted_message)
+
+    return decrypt(encrypted_message, key, casesensitive), key
+
+
+
+def find_key(encrypted_message, maxlen=2000):
+    length = min(maxlen, len(encrypted_message))
+    encrypted_message = encrypted_message[:length]
+    maxkey = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    maxscore = -99e9
+    parentscore, parentkey = maxscore, maxkey[:]
+
+    ctext = re.sub('[^A-Z]', '', encrypted_message.upper())
+    i = 0
+    detext = decrypt(encrypted_message, maxkey)
+
+    while not check_english(detext):
+        i = i + 1
+        random.shuffle(parentkey)
+        deciphered = SimpleSub(parentkey).decipher(ctext)
+        parentscore = fitness.score(deciphered)
+        count = 0
+        while count < 1000:
+            a = random.randint(0, 25)
+            b = random.randint(0, 25)
+            child = parentkey[:]
+            child[a], child[b] = child[b], child[a]
+            deciphered = SimpleSub(child).decipher(ctext)
+            score = fitness.score(deciphered)
+            if score > parentscore:
+                parentscore = score
+                parentkey = child[:]
+                count = 0
+            count = count + 1
+        if parentscore > maxscore:
+            maxscore, maxkey = parentscore, parentkey[:]
+            print('\nbest score so far:', maxscore, 'on iteration', i)
+            print('    best key: ' + ''.join(maxkey))
+            detext = decrypt(encrypted_message, maxkey)
+
+
+    return ''.join(maxkey)
+
+
+    
+    
+
+
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import caesar_cipher as caesar
-import permutation_cipher as permutation
-import substitution_cipher as substitution
-from keyprocess import key_process
-import random_key
-import ngram_score as ns
+import cipher.caesar_cipher as caesar
+import cipher.permutation_cipher as permutation
+import cipher.substitution_cipher as substitution
+from modules.keyprocess import key_process
+import modules.random_key
+import modules.ngram_score as ns
 
 
 
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     # key = key_process(key, 'permutation', False)
 
-    with open('hamlet.txt', 'r') as file:
+    with open('data/hamlet.txt', 'r') as file:
         text = file.read()
 
     # text = "Smith's boyfriend, Andrew Moore, posted a video to her TikTok account Tuesday sharing the news with her followers. The video has gotten over 800,000 likes since being posted on the social media platform.The video has also been commented on over 60,000 times, with many showing support and passing along condolences to Smith's family and loved ones.According to the BBC, Smith had complained of back pain about 10 months before her diagnosis, but it was not until she lost feeling in her left leg that she knew something was wrong."
