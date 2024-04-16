f7f0013804448e09b2c6cda35eec992cac8e8410
Gubin Zhao <gbzhao@outlook.com>
2024-04-16 02:02:07 +0100

    modified comments

--- modules/ngram_score.py
+++ modules/ngram_score.py
@@ -3,27 +3,27 @@ from math import log10
 
 class ngram_score(object):
     def __init__(self, ngramfile, sep=' '):
-        '''加载包含ngrams和计数的文件,计算对数概率'''
-        self.ngrams = {}  # 存储ngrams及其计数的字典
+        # load a file containing ngrams and counts, calculate log probabilities
+        self.ngrams = {}  # store ngrams and their counts
         with open(ngramfile, 'r') as file:
             for line in file:
-                key, count = line.split(sep)  # 将行按分隔符分割为ngram和计数
-                self.ngrams[key] = int(count)  # 将ngram及其计数存储到字典中
-        self.L = len(key)  # ngram的长度
-        self.N = sum(self.ngrams.values())  # 所有ngrams的总计数
-        # 计算对数概率
+                key, count = line.split(sep)  # divide the ngram and its count
+                self.ngrams[key] = int(count)  # store the ngram and its count
+        self.L = len(key)  # length of ngram
+        self.N = sum(self.ngrams.values())  # total number of ngrams
+        # calculate the log probability of ngrams
         for key in self.ngrams.keys():
             self.ngrams[key] = log10(
-                float(self.ngrams[key]) / self.N)  # 计算ngram的对数概率
-        self.floor = log10(0.01 / self.N)  # 用于处理未知的ngrams的默认对数概率阈值
+                float(self.ngrams[key]) / self.N)  # calculate the log probability of ngrams
+        self.floor = log10(0.01 / self.N)  # calculate the log probability threshold
 
     def score(self, text):
-        '''计算文本的分数'''
+        # compute the score of a text
         score = 0
-        ngrams = self.ngrams.__getitem__  # 获取ngrams的对数概率函数
+        ngrams = self.ngrams.__getitem__  # get the value of ngrams
         for i in range(len(text) - self.L + 1):
             if text[i:i+self.L] in self.ngrams:
-                score += ngrams(text[i:i+self.L])  # 若ngrams在文本中存在，则加上对数概率
+                score += ngrams(text[i:i+self.L])  
             else:
-                score += self.floor  # 若ngrams在文本中不存在，则加上默认对数概率阈值
+                score += self.floor  # if the ngram is not in the ngrams, add the threshold
         return score
